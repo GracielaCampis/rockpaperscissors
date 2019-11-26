52e6609db7a46af350413893f2f4a230747cb8e1
GracielaCampis <animallover4278@gmail.com>
2019-11-26 23:26:18 +0000

    Javascript Changes

--- js/script.js
+++ js/script.js
@@ -1,11 +1,17 @@
 // *************************** YOUR CODE BELOW *******************************
 //******************TEST EARLY AND OFTEN USING console.log() ******************
 //****************** SERIOUSLY TEST USING console.log()!!! ******************
+$("#shoot").click(function() {
 
-//GLOBAL VARIABLES
-/* global $ */
-
-
-
-// DOCUMENT READY FUNCTION BELOW
-
+let shootie = Math.random();
+    console.log(shootie);
+    if (shootie > .10) {
+        $("#result").text("paper");
+    }
+    else if(shootie < .20) {
+        $("#result").text("scissors");
+    }
+    else (shootie < .30) {
+        $("#result").text("rock");
+    }
+});
