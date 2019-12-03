395f74dd9a95eea197cab057231e63baadb90429
GracielaCampis <animallover4278@gmail.com>
2019-12-03 23:27:23 +0000

    Changed Code

--- js/script.js
+++ js/script.js
@@ -5,13 +5,17 @@ $("#shoot").click(function() {
 
 let shootie = Math.random();
     console.log(shootie);
-    if (shootie > .10) {
+    if (shootie <= .33) {
         $("#result").text("paper");
     }
-    else if(shootie < .20) {
+    else if(shootie > .66) {
         $("#result").text("scissors");
     }
-    else (shootie < .30) {
+    else {
         $("#result").text("rock");
     }
+
+    console.log("yay")
 });
+//put everything into a "let" and do something like if user === this 
+//and computer Choice === this it'll do it. Do what we have learned. 
