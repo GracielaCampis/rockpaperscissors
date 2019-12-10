47a5c82409b43dea56b7b68abe2412632f542f95
GracielaCampis <animallover4278@gmail.com>
2019-12-10 23:32:56 +0000

    Readable

--- js/script.js
+++ js/script.js
@@ -1,21 +1,61 @@
 // *************************** YOUR CODE BELOW *******************************
 //******************TEST EARLY AND OFTEN USING console.log() ******************
 //****************** SERIOUSLY TEST USING console.log()!!! ******************
-$("#shoot").click(function() {
+//$("#shoot").click(function() {
+//var random=[
+//"Rock","Paper","Scissor"
+//]
+//let type = $(".user").val();
+//let shoot = Math.random();
+  //  console.log(shoot);
+  // if (shoot === "Rock" && computerChoice === "Scissor") {
+  //     $("#result").html("User Wins!");
+  //  }
+  //  else if(shoot === "Paper" && computerChoice === "Rock") {
+  //      $("#result").html("User Wins!");
+   // }
+   // else if (shoot === "Scissor" && computerChoice === "Paper") {
+   //     $("#result").html("User Wins!");
+   // }
+   // else if (shoot === "Rock" && computerChoice === "Paper") {
+    //    $("#result").html("User Loses!");
+   // }
+    //else if (shoot === "Paper" && computerChoice === "Scissor") {
+     //   $("#result").html("User Loses!");
+    //}
+    //else if (shoot === "Scissor" && computerChoice === "Rock"){
+     //   $("#result").html("User Loses!");
+    //}
+    //else {
+      //  $("#result").html("TIE")
+    //}
 
-let shootie = Math.random();
-    console.log(shootie);
-    if (shootie <= .33) {
-        $("#result").text("paper");
+$("#shoot").click(function() {
+    let box = $("#inside").val(); 
+    let computerChoice = getComputerChoice();
+});
+function getComputerChoice() {
+    let randNum = Math.random();
+    if(randNum > 0 && randNum <= .33) {
+        return "Scissors"
     }
-    else if(shootie > .66) {
-        $("#result").text("scissors");
+    else if(randNum > .33 && randNum < .666) {
+         return "Paper"
+    }
+    else if(randNum > .66 && randomNumber > .99) {
+         return "Rock"
     }
     else {
-        $("#result").text("rock");
+        return "Tie"
     }
+}); 
 
-    console.log("yay")
-});
+
+
+
+
+
+    console.log("shoot")
+
 //put everything into a "let" and do something like if user === this 
 //and computer Choice === this it'll do it. Do what we have learned. 
